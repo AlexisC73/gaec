58883487e4505f97775a29dbbb9f3b2183a358dc
Alexis Comte <alexis.comte73@icloud.com>
2024-08-13 00:30:27 +0200

    add error if product exist in other table

--- app/controllers/products/delete_products_controller.ts
+++ app/controllers/products/delete_products_controller.ts
@@ -4,21 +4,28 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DeleteProductController {
   async handle({ auth, response, request }: HttpContext) {
-    const authUser = await auth.authenticate()
-    if (authUser.role !== 'admin') {
-      return response.forbidden({ message: 'You are not authorized to access this resource' })
+    try {
+      const authUser = await auth.authenticate()
+      if (authUser.role !== 'admin') {
+        return response.forbidden({ message: 'You are not authorized to access this resource' })
+      }
+      const { id: productId } = request.params()
+
+      const validateData = await deleteProductValidator.validate({ productId })
+
+      const product = await Product.findBy('id', validateData.productId)
+
+      if (!product) {
+        return response.notFound({ message: 'Product not found' })
+      }
+
+      await product.delete()
+      return response.noContent()
+    } catch (e) {
+      if (e.code === '23503') {
+        return response.badRequest({ message: 'Product is being used in other tables' })
+      }
+      return e
     }
-    const { id: productId } = request.params()
-
-    const validateData = await deleteProductValidator.validate({ productId })
-
-    const product = await Product.findBy('id', validateData.productId)
-
-    if (!product) {
-      return response.notFound({ message: 'Product not found' })
-    }
-
-    await product.delete()
-    return response.noContent()
   }
 }
